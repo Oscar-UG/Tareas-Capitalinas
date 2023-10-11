2f16c85f4a483039e47108643fdba3aa231f9ed3
Oscar Javier Urrutia Gutierrez <oscar.urrutia@jala.university>
2023-10-10 20:28:39 -0500

    Rutas basicas definidas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Main Page</h1>} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/tasks" element={<h1>Tasks</h1>} />
+        <Route path="/add-taks" element={<h1>New Task</h1>} />
+        <Route path="/tasks/:id" element={<h1>Hello world</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
