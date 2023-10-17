b700eab9ea75427564d59e937998d634a6772bcc
Jhoseb <Jose.Moya@jala.university>
2023-10-17 15:01:19 -0500

    cambios en la apariencia

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -5,11 +5,9 @@ function MainPage() {
     <div className="hero min-h-screen">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
+          <h1 className="text-5xl font-bold">Bienvenido</h1>
           <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Con tareas capitalinas podras hacer un mejor control de tu tiempo y vida :3
           </p>
           <Link to={"/register"}>
             <button className="btn btn-primary">Continua</button>
